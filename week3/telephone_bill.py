5a072215cfc258f774d37c793705db84220b779a
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-10-20 11:41:12 +0100

    c

---
+++ week3/telephone_bill.py
@@ -0,0 +1,16 @@
+# Program that calculate telephone bill
+print("\nWelcome to exercise 1 (week - 3) of SDAM")
+
+# variables
+minutes = int(input("\nType the quantity of minutes used: "))
+
+# calculations
+call_charge = float((minutes * 0.15))
+vat = float((call_charge * 0.20))
+total_bill = float((call_charge + vat))
+
+# output data
+print("\nNumber of minutes used: {}" .format(minutes))
+print("Basic call charge: £ {:.2f}" .format(call_charge))
+print("VAT due: £ {:.2f}" .format(vat))
+print("Total bill: £ {:.2f}" .format(total_bill))
