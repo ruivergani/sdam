2a9af766fb2a043ffd3e06ebe9a1f30a1f4b77f7
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-11-03 21:00:04 +0000

    week 5 - expert exercises

---
+++ week5/exercise10.py
@@ -0,0 +1,9 @@
+# exercise in linux (logic)
+
+
+answer1 = 
+
+n = 0
+while  n < 3:
+
+    n = n + 1
--- week5/paper_rock_scissors.py
+++ week5/paper_rock_scissors.py
@@ -3,8 +3,43 @@
 import math
 import random
 
-print("\nRocket / Paper / Scissors Game")
-user_choice = input("Type your choice: ")
+print("\nRock / Paper / Scissors ")
+score_user = 0
+score_computer = 0
 
-computer_choice = str(random.choice(['Rocket', 'rocket', 'paper', 'Paper', 'Scissors', 'scissors']))
-print("Computer choice: {}" .format(computer_choice))
+while (score_user < 3) or (score_computer < 3):
+    # user choice
+    user_choice = input("\nEnter your selection: ").strip().lower()
+    print("You chose: {}".format(user_choice))
+
+    # computer choice generated randomly
+    computer_choice = random.choice(['rock', 'paper', 'scissors'])
+    print("The computer chose: {}".format(computer_choice))
+
+    # variables for message
+    computer_wins = "The computer won!"
+    you_win = "Congratulations, you win!"
+
+    # computer wins
+    if (user_choice == 'scissors' and computer_choice == 'rock') or \
+            (user_choice == 'paper' and computer_choice == 'scissors') or \
+            (user_choice == 'rock' and computer_choice == 'paper'):
+        print(computer_wins)
+        score_computer += 1
+        print("Computer score is: {}".format(score_computer))
+
+    # user wins
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+            (user_choice == 'scissors' and computer_choice == 'paper') or \
+            (user_choice == 'paper' and computer_choice == 'rock'):
+        print(you_win)
+        score_user += 1
+        print("Your score is: {}".format(score_user))
+    else:
+        if user_choice == computer_choice:
+            print("It's a draw.")
+            score_user += 0
+            score_computer += 0
+        else:
+            continue
+print("Game over.")
