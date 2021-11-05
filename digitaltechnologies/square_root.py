568d3934f7b2b58f9ea6b7d8df554aa54c0095c3
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-11-05 12:27:38 +0000

    week 5 - digital technologies

---
+++ digitaltechnologies/square_root.py
@@ -0,0 +1,9 @@
+# square root exercise
+
+import math
+
+number = int(input("Type a number: "))
+
+# calculate square root
+
+print("Square root: ", math.sqrt(number))
--- week5/paper_rock_scissors.py
+++ week5/paper_rock_scissors.py
@@ -4,10 +4,10 @@ import math
 import random
 
 print("\nRock / Paper / Scissors ")
-score_user = 0
-score_computer = 0
+score_user = 3
+score_computer = 3
 
-while (score_user < 3) or (score_computer < 3):
+while score_user > 0 or score_computer > 0:
     # user choice
     user_choice = input("\nEnter your selection: ").strip().lower()
     print("You chose: {}".format(user_choice))
@@ -21,25 +21,19 @@ while (score_user < 3) or (score_computer < 3):
     you_win = "Congratulations, you win!"
 
     # computer wins
-    if (user_choice == 'scissors' and computer_choice == 'rock') or \
-            (user_choice == 'paper' and computer_choice == 'scissors') or \
-            (user_choice == 'rock' and computer_choice == 'paper'):
+    if (user_choice == 'scissors' and computer_choice == 'rock') or (user_choice == 'paper' and computer_choice == 'scissors') or (user_choice == 'rock' and computer_choice == 'paper'):
         print(computer_wins)
-        score_computer += 1
+        score_computer = score_computer - 1
         print("Computer score is: {}".format(score_computer))
 
     # user wins
-    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
-            (user_choice == 'scissors' and computer_choice == 'paper') or \
-            (user_choice == 'paper' and computer_choice == 'rock'):
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or (user_choice == 'scissors' and computer_choice == 'paper') or (user_choice == 'paper' and computer_choice == 'rock'):
         print(you_win)
-        score_user += 1
+        score_user = score_user - 1
         print("Your score is: {}".format(score_user))
+
     else:
         if user_choice == computer_choice:
             print("It's a draw.")
-            score_user += 0
-            score_computer += 0
-        else:
-            continue
+
 print("Game over.")
