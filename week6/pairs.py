5cc50eb36ef4d6cf6a9e82aef73ad1f99f4389e3
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-11-22 21:30:05 +0000

    pairs exercise done

--- week6/pairs.py
+++ week6/pairs.py
@@ -5,7 +5,6 @@ import random
 
 print("\nWelcome to the pairs exercise.\n")
 
-# card face up
 card_face_up = [["Jack", "King", "Ace", "Queen"],
                 ["Jack", "King", "Ace", "Queen"],
                 ["Jack", "King", "Ace", "Queen"],
@@ -15,7 +14,13 @@ card_face_down = [[" * ", " * ", " * ", " * "],
                   [" * ", " * ", " * ", " * "],
                   [" * ", " * ", " * ", " * "],
                   [" * ", " * ", " * ", " * "]]
-# card face up
+
+game_over = [[" X ", " X ", " X ", " X "],
+             [" X ", " X ", " X ", " X "],
+             [" X ", " X ", " X ", " X "],
+             [" X ", " X ", " X ", " X "]]
+
+# display both set of cards
 j = 0
 print("Face Up: ")
 print("   0    1     2    3")
@@ -24,8 +29,6 @@ for i in range(len(card_face_up)):
     for j in range(len(card_face_up[i])):
         print(card_face_up[i][j], end=" ")
     print()
-
-# card face down
 print("\nFace Down: ")
 print("   0   1   2   3")
 for i in range(len(card_face_down)):
@@ -34,37 +37,34 @@ for i in range(len(card_face_down)):
         print(card_face_down[i][j], end=" ")
     print()
 
-# first card input
-row = int(input("\nEnter a card position (row): "))
-column = int(input("Enter a card position (column): "))
-# print first card
-print("\nFirst Card chosen: ", card_face_up[row][column])
-# second card input
-x = int(input("\nEnter second card to compare (row): "))
-y = int(input("Enter second card to compare (column): "))
-# print second card
-print("\nSecond Card chosen: ", card_face_up[x][y])
-
-while card_face_up[row][column] != card_face_up[x][y]:
-    print("\nNo match found.")
-    # request from user
+while card_face_up != game_over:
+    # display user input (no validation)
     row = int(input("\nEnter a card position (row): "))
     column = int(input("Enter a card position (column): "))
-    # access an index (first card)
     print("\nFirst Card chosen: ", card_face_up[row][column])
     x = int(input("\nEnter second card to compare (row): "))
     y = int(input("Enter second card to compare (column): "))
-    # access an index (second card)
     print("\nSecond Card chosen: ", card_face_up[x][y])
-else:
-    print("You have matched values. ")
-    j = 0
-    card_face_up[row][column] = "X"
-    card_face_up[x][y] = "X"
-    print("\nNew layout: ")
-    print("   0    1     2    3")
-    for i in range(len(card_face_up)):
-        print(i, end=" ", sep=" ")
-        for j in range(len(card_face_up[i])):
-            print(card_face_up[i][j], end=" ")
-        print()
+
+    while card_face_up[row][column] != card_face_up[x][y]:
+        print("\nNo match found.")
+
+        # display user input (no validation)
+        row = int(input("\nEnter a card position (row): "))
+        column = int(input("Enter a card position (column): "))
+        print("\nFirst Card chosen: ", card_face_up[row][column])
+        x = int(input("\nEnter second card to compare (row): "))
+        y = int(input("Enter second card to compare (column): "))
+        print("\nSecond Card chosen: ", card_face_up[x][y])
+    else:
+        print("You have matched values. ")
+        j = 0
+        card_face_up[row][column] = " X "
+        card_face_up[x][y] = " X "
+        print("\nNew layout: ")
+        print("  0   1    2   3")
+        for i in range(len(card_face_up)):
+            print(i, end=" ", sep=" ")
+            for j in range(len(card_face_up[i])):
+                print(card_face_up[i][j], end=" ")
+            print()
--- week6/pairs2.py
+++ week6/pairs2.py
@@ -1,58 +1,36 @@
-# ARAN (Student) code in order to analyse and share knowledge
+# pairs exercise
 
-card_hide = [["*","*","*","*"],["*","*","*","*"],["*","*","*","*"],["*","*","*","*"]]
-card_show = [["J","Q","K","A"],["A","J","Q","K"],["K","A","J","Q"],["Q","K","A","J"]]
+import math
+import random
 
+print("\nWelcome to the pairs exercise.\n")
 
-#for x in range (0,4):
-   # for y in range(0,4):
-  #      print(card_show[x][y], end= "\t")
- #       if(x == 4):
-#            print("\n")
+# card face up
+card_face_up = [["Jack", "King", "Ace", "Queen"],
+                ["Jack", "King", "Ace", "Queen"],
+                ["Jack", "King", "Ace", "Queen"],
+                ["Jack", "King", "Ace", "Queen"]]
 
-for r in card_hide:
-    for c in r:
-        print(c,end=" ")
+card_face_down = [[" * ", " * ", " * ", " * "],
+                  [" * ", " * ", " * ", " * "],
+                  [" * ", " * ", " * ", " * "],
+                  [" * ", " * ", " * ", " * "]]
+# card face up
+j = 0
+print("Face Up: ")
+print("   0    1     2    3")
+for i in range(len(card_face_up)):
+    print(i, end=" ", sep=" ")
+    for j in range(len(card_face_up[i])):
+        print(card_face_up[i][j], end=" ")
     print()
 
-##row left to right
-##colums up and down
-## [0][1] is Q - So [a][b] a columns - {so y} ; b row - {so x}
-print(card_show[0][1])
+# card face down
+print("\nFace Down: ")
+print("   0   1   2   3")
+for i in range(len(card_face_down)):
+    print(i, end=" ", sep=" ")
+    for j in range(len(card_face_down[i])):
+        print(card_face_down[i][j], end=" ")
+    print()
 
-
-while(card_show != card_hide):
-    print("Choice 1: ")
-    c1_c = int(input("Enter the column :"))
-    c1_r = int(input("Enter the row :"))
-
-    hold_pos1 = card_show[c1_c][c1_r]
-
-    print("Choice 2: ")
-    c2_c = int(input("Enter the column :"))
-    c2_r = int(input("Enter the row :"))
-
-    hold_pos2 = card_show[c2_c][c2_r]
-
-    if (card_show[c1_c][c1_r] == card_show[c2_c][c2_r]):
-        print("Duplicate")
-        card_hide[c1_c][c1_r] = card_show[c2_c][c2_r]
-        card_hide[c2_c][c2_r] = card_show[c1_c][c1_r]
-
-        if (hold_pos1 == "J"):
-            print("Two Jacks")
-            #card_hide[c1_c][c1_r] = card_show[c1_c][c1_r]
-        elif (hold_pos1 == "K"):
-            print("Two Kings")
-            #card_hide[c1_c][c1_r] = card_show[c1_c][c1_r]
-        elif (hold_pos1 == "Q"):
-            print("Two Queens")
-            #card_hide[c1_c][c1_r] = card_show[c1_c][c1_r]
-        elif (hold_pos1 == "A"):
-            print("Two Aces")
-            #card_hide[c1_c][c1_r] = card_show[c1_c][c1_r]
-
-    for r in card_hide:
-            for c in r:
-                print(c, end=" ")
-            print()
