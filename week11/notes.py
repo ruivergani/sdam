2f435d0cfc822e7575d7378a96235234466240c7
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-12-27 17:54:10 +0000

    notes revision

---
+++ week11/notes.py
@@ -0,0 +1,25 @@
+# week 1
+# P E M D A S = () ** * / + -
+# // floor division (int value)
+# % rest of division
+# / = type special character | '' or "" or """ """ as long is consistent
+# print(sep=''  separate objects, end='\n' default new line, file=sys.stdout sent to default stdout, flush=False force buffer)
+
+# week 2
+# .capitalize() = first letter capital
+# .title() =
+# .format() = format to string
+# .join() = join iterables
+# .lower() = lowercase
+# .rjust() = right justified
+# .ljust() = left justified
+# .split() = split string
+# input holds String value | remember python only prints String value (str(round,2) for example
+
+# week 3
+# Sequence: step by step
+# Iteration: repetitive statement = loop (do, while, for)
+# Selection = choose (if/elif/else)
+# syntax error (machine error or code) | logical error (does not give message, executes, but result is wrong) | runtime error (division 0)
+# Test plan: data | reason | expected | actual
+# Debug is the process of removing errors
